66d699d458d1e3d4e68fd24829905c567615b4a7
phailycs <felix.robin--mores@hotmail.com>
2024-11-11 20:38:27 +0100

    changement de stratégie.

--- backend/src/api/agents.js
+++ backend/src/api/agents.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const { fetchAgents } = require('../dataFetcher');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const router = express.Router();
 
+const fetchAgents = async () => {
+  try {
+    const query = `
+      SELECT name, icon, playable FROM agents;
+    `;
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (error) {
+    console.error('Erreur récup données agents:', error);
+    throw error;
+  }
+};
+
 router.get('/', async (req, res) => {
-    try {
-      const agents = await fetchAgents();
-      res.json(agents);
-    } catch (error) {
-      res.status(500).send('Erreur serveur');
-    }
-  });
+  try {
+    const agents = await fetchAgents();
+    res.json(agents);
+  } catch (error) {
+    res.status(500).send('Erreur serveur');
+  }
+});
 
 module.exports = router;
--- backend/src/api/weapons.js
+++ backend/src/api/weapons.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const { fetchWeapons } = require('../dataFetcher');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const router = express.Router();
 
+const fetchWeapons = async () => {
+  try {
+    const query = `
+      SELECT name, icon, category, cost FROM weapons;
+    `;
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (error) {
+    console.error('Erreur récup données armes:', error);
+    throw error;
+  }
+};
+
 router.get('/', async (req, res) => {
-    try {
-      const weapons = await fetchWeapons();
-      res.json(weapons);
-    } catch (error) {
-      res.status(500).send('Erreur serveur');
-    }
-  });
+  try {
+    const weapons = await fetchWeapons();
+    res.json(weapons);
+  } catch (error) {
+    res.status(500).send('Erreur serveur');
+  }
+});
 
 module.exports = router;
