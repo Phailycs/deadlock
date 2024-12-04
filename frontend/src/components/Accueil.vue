5d67bb4a404ae338b894866fca2ee464f3370dc1
selma <selma.eljabri1@gmail.com>
2024-12-04 12:57:12 +0100

    Retour à version antérieure

--- frontend/src/components/Accueil.vue
+++ frontend/src/components/Accueil.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-col items-center justify-center min-h-screen text-white">
 
     <!-- Image Valorant Randomizer en haut à gauche -->
-    <div class="container flex">
+    <div class="absolute mb-8 flex justify-center pb-96 ">
       <img src="../assets/img/valorant_randomizer.svg" alt="Valorant Randomizer" class="img_valo" />
     </div>
 
-    <div class="container flex space-x-12">
+    <div class="flex space-x-12 pb-2 pt-24">
       <button @click="$router.push('/selectAgents')" class="standard-button font-third-font font-bold">
         <div>
             <div>
@@ -27,78 +27,20 @@
 </template>
 
 <style scoped>
-
-/* mb-xs */
-@media (min-width: 320px) {
-  .img_valo {
-    width: 17rem;
-    
-  }
-
-  .flex {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-/* xs */
-@media (min-width: 475px) {
-  .img_valo {
-    width: 17rem;
-    margin-bottom: 2rem;
-  }
-
-  .flex {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-/* sm */
-@media (min-width: 640px) {
-  .img_valo {
-    width: 17rem;
-    margin-bottom: 2rem;
-  }
-
-  .flex {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-/* md */
-@media (min-width: 768px) {
-  .img_valo {
-    width: 17rem;
-    margin-bottom: 2rem;
-  }
-
-  .flex {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-/* lg */
-@media (min-width: 1024px) {
-  .img_valo {
-    width: 17rem;
-    margin-bottom: 2rem;
-  }
-}
-
-/* xl */
-@media (min-width: 1280px) {
-  .img_valo{
+.img_valo{
   width: 36rem;
-  }
 }
 
-/* 2xl */
-@media (min-width: 1536px) {
-  .img_valo{
-  width: 36rem;
- }
+/* Media query pour les dimensions 425 x 841 */
+@media (max-width: 425px) and (max-height: 841px) {
+  .img_valo {
+    width: 17rem;
+    margin-bottom: 2rem;
+  }
+
+  .flex {
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
