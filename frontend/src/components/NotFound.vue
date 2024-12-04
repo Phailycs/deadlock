9c74b72b6cf9797cb2b705daf4ae7af0e33e2097
selma <selma.eljabri1@gmail.com>
2024-12-04 13:06:03 +0100

    Ajout d'une page 404

---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page not found</h1>
+    <p>Oops, we couldn't find the page you were looking for.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 50px;
+  background-color: #f4f4f4;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+
+h1 {
+  font-size: 3em;
+  color: #eb565b;
+}
+
+p {
+  font-size: 1.5em;
+  color: #333;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,10 +3,16 @@ import Accueil from '../components/Accueil.vue';
 import SelectNumberAgent from '../components/SelectNumberAgent';
 import SelectMoneyWeapons from '../components/SelectMoneyWeapons.vue';
 import RandomWeaponsSelector from '../components/RandomWeaponsSelector.vue';
+import NotFound from '../components/NotFound.vue';
 
 const routes = [
   {
     path: '/',
+    name: 'Accueil',
+    component: Accueil,
+  },
+  {
+    path: '/accueil',
     name: 'Accueil',
     component: Accueil,
   },
@@ -24,6 +30,11 @@ const routes = [
     path: '/randomWeapons/:cost',
     name: 'RandomWeaponsSelector',
     component: RandomWeaponsSelector,
+  },
+  {
+    path: '/:pathMatch(.*)*', // Cette route capture toutes les URL non définies
+    name: 'NotFound',
+    component: NotFound,
   }
 ];
 
